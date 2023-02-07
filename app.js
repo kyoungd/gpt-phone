327e6334e62b5b5b80037f0617ea5c465450d8ae
kyoungd <kyoungd@hotmail.com>
2023-02-06 21:55:14 -0800

    I added catch and retry for openAI failure.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const GetNextMessage = require('./getNextMessage.js');
 require('dotenv').config()
 app.use(urlencoded({ extended: false }));
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const voice = { voice: 'Polly.Kimberly', language: 'en-US' };
+const voice = { voice: 'Polly.Salli-Neural', language: 'en-US' };
 
 let phoneCallState;
 
@@ -20,20 +20,6 @@ app.post("/voice", async (req, res) => {
     const message = phoneCallState.LastMessage;
 
     res.set("Content-Type", "text/xml");
-    // res.send(
-    //   `<Response>
-    //      <Say voice="Polly.Joanna">
-    //        This is Joanna from Accident Specialists.  How can I help you?
-    //      </Say>
-    //      <Gather
-    //           input="speech"
-    //           action="/acknolwedge"
-    //           language= "en-US"
-    //           speechModel="phone_call"
-    //           speechTimeout="2"
-    //      />
-    //    </Response>`
-    // );
     const twiml = new VoiceResponse();
     const gather = twiml.gather({
         input: 'speech',
@@ -64,22 +50,37 @@ app.post("/voice", async (req, res) => {
 
     // process response from user and get the next question.
     const result1 = await GetNextMessage(phoneCallState.State, userInput)
-    phoneCallState.State = result1.data;
+    if (result1.status === 200) {
+      try {
+        phoneCallState.State = result1.data;
 
-    console.log(phoneCallState.Status);
-
-    const message = phoneCallState.LastMessage;
-    const gather = twiml.gather({
-        input: 'speech',
-        action: '/ack',
-        language: 'en-US',
-        speechModel: 'phone_call',
-        speechTimeout: phoneCallState.SpeechTimeout
-    });
-    gather.say(voice, message);
-    if (phoneCallState.IsPhoneCallEnd)
-      twiml.hangup();
-    res.send(twiml.toString());
+        console.log(phoneCallState.Status);
+    
+        const message = phoneCallState.LastMessage;
+        const gather = twiml.gather({
+            input: 'speech',
+            action: '/ack',
+            language: 'en-US',
+            speechModel: 'phone_call',
+            speechTimeout: phoneCallState.SpeechTimeout
+        });
+        gather.say(voice, message);
+        if (phoneCallState.IsPhoneCallEnd)
+          twiml.hangup();
+        res.send(twiml.toString());    
+      }
+      catch (err) {
+        console.log(err);
+        twiml.say(voice, 'Hold on.  Give me a second here.');
+        twiml.redirect({method: 'POST'}, `${process.env.SELF_URL}/answer`);
+        res.send(twiml.toString());
+      }
+    }
+    else {
+      twiml.say(voice, 'Hold on.  Give me a second here.');
+      twiml.redirect({method: 'POST'}, `${process.env.SELF_URL}/answer`);
+      res.send(twiml.toString());
+    }
   });
 
   app.get('/ping', (req, res) => {
